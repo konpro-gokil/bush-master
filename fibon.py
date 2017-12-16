0267ce18ff164018a196b0b1417cced2c798fc09
Adjipras17 <mochadjiprasetyo1997@gmail.com>
2017-12-16 12:36:34 +0700

    saya nambah file

---
+++ fibon.py
@@ -0,0 +1,17 @@
+list=[]
+
+def fib(n):
+    if n==0:
+        return 0
+	if n==1 :
+		return 1
+    else:
+        return (fib(n-1)+fib(n-2)) 
+
+		
+n = int (input("masukkan banyak bilangan fibnacci :")) 
+#print(fib(n))
+for i in range (n): 
+	list.append(fib(i)) 
+
+print(list)
